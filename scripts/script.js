0da74c961d0874eeac3a975b32190b7970fc9d25
Daniel <83757480+DeeAyDan@users.noreply.github.com>
2023-09-07 10:25:15 +0200

    16x16 square grid styled

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 function makeGrid(){
     const container = document.getElementById('drawingGrid');
-    for(let i = 0;i<=16;i++){
-        for(let j = 0;j<=16;j++){
-            const newDiv = document.createElement(`div`);
-            container.appendChild(newDiv);
-        }
+    let itemCount = 16 * 16;
+    for(let i = 0;i<itemCount;i++){
+        const newDiv = document.createElement(`div`);
+        newDiv.classList.add(`fieldGridSquare`)
+        container.appendChild(newDiv);
     }
 }
 
