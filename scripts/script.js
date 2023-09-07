a34fbc0d6a91cbbe1228925825289552d0ab0158
Daniel <83757480+DeeAyDan@users.noreply.github.com>
2023-09-07 12:35:43 +0200

    Added button to change the size of the playing field

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 function makeGrid(size){
     const container = document.getElementById('drawingGrid');
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     let itemCount = size * size;
     for(let i = 0;i<itemCount;i++){
         const newDiv = document.createElement(`div`);
@@ -11,6 +12,20 @@ function makeGrid(size){
 
 function changeBackgroundColor(element){
     element.style.backgroundColor = "black";
+}
+
+function deleteGrid(){
+    const squaresToDelete = document.querySelectorAll('.fieldGridSquare');
+    squaresToDelete.forEach(function (element){
+        element.parentNode.removeChild(element);
+    });
+}
+
+function changeSize(){
+    const userInput = prompt(`Please enter the new size of the grid.`)
+    deleteGrid();
+    makeGrid(userInput);
+
 }
 
 makeGrid(16);
